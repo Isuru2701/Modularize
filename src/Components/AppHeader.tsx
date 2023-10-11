77a6458e95445855c78b3139a08581ebe90c2fe3
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-11 10:06:47 +0530

    adding styles.css and changing appheader

--- src/Components/AppHeader.tsx
+++ src/Components/AppHeader.tsx
@@ -1,10 +1,11 @@
 import { Container } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import BusinessName from "./BusinessName";
 
 export default function AppHeader() {
   return (
-    <>
-      <header>
+    <div>
         <Navbar expand="lg" bg="dark" data-bs-theme="dark" fixed="top">
           <Container fluid>
             <Navbar.Brand href="#home">
@@ -12,12 +13,11 @@ export default function AppHeader() {
                 src="icon.svg"
                 width="50"
                 className="d-inline-block align-top"
-              />
+              /><BusinessName/> {" "}
               
             </Navbar.Brand>
           </Container>
         </Navbar>
-      </header>
-    </>
+    </div>
   );
 }
