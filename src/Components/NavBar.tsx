bb1213b8e4d5c05f7a66678324697d2437875847
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-11 09:33:10 +0530

    adding navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,2 +1,18 @@
-import React from 'react';
-import { Nav}
+import { Container } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function NavBar() {
+  return (
+    <>
+      <Navbar className="bg-body-tertiary">
+        <Container>
+            <Navbar.Brand href="#home"/>
+            <img
+            src="public/icon.svg"
+            width="30"
+            />
+        </Container>
+      </Navbar>
+    </>
+  );
+}
