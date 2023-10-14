f3a225fd348d8c9052be9dbc3ddc5b8fd52ecff1
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-14 13:56:13 +0530

    removed businessname.tsx

--- src/components/BusinessName.tsx
+++
@@ -1,8 +0,0 @@
-import "../styles/header.css";
-
-export default function BusinessName() {
-    const businessName = "Business Name";
-    return (
-        <div id="businessName">{businessName}</div>
-    );   
-}
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,10 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-
+import "../styles/header.css";
 function BrandExample() {
   return (
     <>
-      <Navbar className="bg-body-tertiary" >
+      <Navbar id="header" className="bg-body-tertiary" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">
             <img
@@ -14,7 +14,7 @@ function BrandExample() {
               height="30"
               className="d-inline-block align-top"
             />{' '}
-            React Bootstrap
+            Modularize
           </Navbar.Brand>
         </Container>
       </Navbar>
